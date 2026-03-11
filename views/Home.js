d37776e2317cb63810e5fd1150877d3cf22f6115
Jim OBrien <jobrien1956@gmail.com>
2026-03-11 13:58:02 -0400

    Add files via upload

--- views/Home.js
+++ views/Home.js
@@ -20,8 +20,11 @@ const mainHeader = {
 		if (!HA.stores.hapages) return;
 		return m("div.container",
 
-			// Site banner
-			m("header.intro-header[id='topContainer']",
+			// Site banner — background image set inline so path is always correct
+			m("header.intro-header", {
+				id: 'topContainer',
+				style: {'background-image': 'url(img/index/having_adventures2.jpg)'}
+			},
 				m("div.row",
 					m("div.col-lg-8.col-lg-offset-2.col-md-10.col-md-offset-1",
 						m("div.site-heading", [
@@ -46,23 +49,32 @@ const mainHeader = {
 			),
 
 			// Trip card grid
-			// Each card's storeName routes to /trip/<storeName>
-			// Trip.js loads json/ha-<storeName>.json
-			// If that JSON has haPages[] (Type 2 index) TripIndex.js takes over
-			// If it has adventureDay[] (Type 1) Trip.js renders it directly
+			// FIX: each card column needs position:relative so that the
+			// absolutely-positioned .square-text and .square-img are contained
+			// FIX: children wrapped in [] array so all three render
 			m("div.row",
 				HA.stores.hapages.haPages
 					.filter(it => it.storeName)
-					.map((it) => m(".col-xs-6.col-sm-4.col-md-3", {key: it.storeName},
-						m(".margin-top"),
-						m("a.square-text", {
-							onclick: () => {
-								console.log('navigate to trip:', it.storeName);
-								m.route.set('/trip/' + it.storeName);
-							}
-						}, it.pageTitle),
-						m(".square-img", {style: {"background-image": it.tripThumb ? "url(" + it.tripThumb + ")" : "none", "background-color": it.tripThumb ? "" : "#2a2a28"}})
-					))
+					.map((it) => m(".col-xs-6.col-sm-4.col-md-3", {
+							key: it.storeName,
+							style: {position: 'relative', height: '150px', marginBottom: '10px'}
+						}, [
+							m("a.square-text", {
+								onclick: () => {
+									console.log('navigate to trip:', it.storeName);
+									m.route.set('/trip/' + it.storeName);
+								}
+							}, it.pageTitle),
+							m(".square-img", {
+								style: {
+									'background-image': it.tripThumb ? 'url(' + it.tripThumb + ')' : 'none',
+									'background-color': it.tripThumb ? '' : '#2a2a28',
+									'background-size': 'cover',
+									'background-position': 'center'
+								}
+							})
+						])
+					)
 			)
 		);
 	}
