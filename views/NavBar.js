3dc340301502bd77fb2e68959c38f7193f9f4d25
Jim OBrien <jobrien1956@gmail.com>
2026-03-20 12:28:36 -0400

    Update NavBar links to use hash routing

--- views/NavBar.js
+++ views/NavBar.js
@@ -1,21 +1,19 @@
 NavBar = {
 	view: function (vnode) {
 		if (!HA.stores.hapages) return;
+
 		const navGroups = HA.stores.hapages.haNavIndex;
 		if (!navGroups) {
 			console.error('NavBar: haNavIndex missing from haPagesNav.json');
 			return;
 		}
+
 		return m("nav.navbar.navbar-inverse.navbar-fixed-top[role='navigation']",
 			m("div.container", {style: {"background-color": "black"}},
 				[
 					m("div.navbar-header", [
 						m("a.navbar-brand", {
-							href: m.route.prefix + '/',
-							onclick: (e) => {
-								e.preventDefault();
-								m.route.set('/');
-							},
+							href: '#!/',
 							style: {color: "gold"}
 						}, "Having Adventures.com"),
 						m("button.navbar-toggle[type='button'][data-toggle='collapse'][data-target='#ha-navbar-collapse']",
@@ -23,24 +21,27 @@ NavBar = {
 							 m("span.icon-bar"), m("span.icon-bar"), m("span.icon-bar")]
 						)
 					]),
+
 					m(".collapse.navbar-right.navbar-collapse[id='ha-navbar-collapse']",
 						m("ul.nav.navbar-nav.navbar-collapse",
 							navGroups.map((grp) => {
 								return m("li.dropdown", {style: {"display": "inline-flex"}},
-									m("a.dropdown-toggle[data-toggle='dropdown'][href='#']",
-										grp.grpName, m("b.caret")
-									),
+									m("a.dropdown-toggle", {
+										'data-toggle': 'dropdown',
+										href: '#',
+										onclick: function(e) { e.preventDefault(); }
+									}, grp.grpName, m("b.caret")),
 									m("ul.dropdown-menu",
 										grp.grpMember.map((member) => {
-											// Skip entries still lacking a storeName
 											if (!member.storeName) return null;
 											return m("li",
 												m("a", {
-													href: m.route.prefix + '/trip/' + member.storeName,
-													onclick: (e) => {
+													href: '#!/trip/' + member.storeName,
+													onclick: function(e) {
 														e.preventDefault();
-														console.log('nav -> trip:', member.storeName);
-														m.route.set('/trip/' + member.storeName);
+														var sn = member.storeName;
+														if (window.$) $(this).closest('.open').removeClass('open');
+														m.route.set('/trip/' + sn);
 													}
 												}, member.name)
 											);
